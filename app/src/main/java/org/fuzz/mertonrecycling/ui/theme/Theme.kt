be4aebe0d97a4b539e579fe113c244f76ad1408e
Jie Qiang Li <jieqiang.li@gmail.com>
2023-12-24 17:09:47 +0000

    fix lint

--- app/src/main/java/org/fuzz/mertonrecycling/ui/theme/Theme.kt
+++ app/src/main/java/org/fuzz/mertonrecycling/ui/theme/Theme.kt
@@ -1,7 +1,6 @@
 package org.fuzz.mertonrecycling.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
