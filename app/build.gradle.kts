e402f0cbbc57832ec665212144e9edc3003a418a
Jie Qiang Li <jieqiang.li@gmail.com>
2024-09-05 13:41:15 +0100

    Bump version for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.fuzz.mertonrecycling"
         minSdk = 33
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
