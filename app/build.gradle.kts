36579a2d0473ab15af91d7fc3de84bc89f5812a8
Jie Qiang Li <jieqiang.li@gmail.com>
2025-07-19 18:35:21 +0100

    Show URL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.fuzz.mertonrecycling"
         minSdk = 33
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- app/src/main/java/org/fuzz/mertonrecycling/MainActivity.kt
+++ app/src/main/java/org/fuzz/mertonrecycling/MainActivity.kt
@@ -7,7 +7,10 @@ import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -16,13 +19,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import org.fuzz.mertonrecycling.ui.theme.MertonRecyclingTheme
 
-// GET https://myneighbourhood.merton.gov.uk/Wasteservices/WasteServices.aspx?uprn=48053547&Address=9
+// GET https://myneighbourhood.merton.gov.uk/Wasteservices/WasteServices.aspx?uprn=48053547
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,10 +35,7 @@ class MainActivity : ComponentActivity() {
             MertonRecyclingTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Column {
-                        Input("Support coming")
-                        Content()
-                    }
+                    RecyclingScreen()
                 }
             }
         }
@@ -41,41 +43,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Input(name: String) {
-    var text by remember { mutableStateOf(name) }
-
-    TextField(
-        value = name,
-        onValueChange = { text = it },
-        label = { Text("Postcode") }
-    )
-
+fun RecyclingScreen() {
+    var url by rememberSaveable { mutableStateOf("https://myneighbourhood.merton.gov.uk/Wasteservices/WasteServices.aspx?uprn=48053547") }
+    Content(url = url, onUrlChange = { url = it })
 }
 
 @Composable
-fun Content() {
-    val url = "https://myneighbourhood.merton.gov.uk/Wasteservices/WasteServices.aspx?uprn=48053547"
-    AndroidView(factory = {
-        WebView(it).apply {
-            layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
+fun Content(url: String, onUrlChange: (String) -> Unit) {
+    Column {
+        val padding = 4.dp
+        Row {
+            var text by remember { mutableStateOf(url) }
+
+            TextField(
+                value = text,
+                onValueChange = { text = it },
+//                label = { Text("URL") }
             )
-            webViewClient = WebViewClient()
-            loadUrl(url)
+
+//            ElevatedButton(
+//                modifier = Modifier.padding(padding),
+//                onClick = { onUrlChange(text) }) {
+//                Text("Search")
+//            }
         }
-    }, update = {
-        it.loadUrl(url)
-    })
+
+        AndroidView(factory = {
+            WebView(it).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                webViewClient = WebViewClient()
+                loadUrl(url)
+            }
+        }, update = {
+            it.loadUrl(url)
+        })
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MertonRecyclingTheme {
-        Column {
-            Input("Support coming")
-            Content()
-        }
+        RecyclingScreen()
     }
 }
